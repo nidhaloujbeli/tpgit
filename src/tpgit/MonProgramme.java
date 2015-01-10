61eef079d5f70b131cfbb8c32efbdd6fba9a24d2
Djebali Nidhal <nidhal.djebali@telecom-paristech.fr>
2015-01-10 14:33:09 +0100

    sbe7 l5ir

--- src/tpgit/MonProgramme.java
+++ src/tpgit/MonProgramme.java
@@ -3,7 +3,7 @@ package tpgit;
 public class MonProgramme {
 	public static void main(String[]args)
 	{
-		System.out.println("Bom dia!");
+		System.out.println("Sbe7 l5ir!");
 		Avion a1= new Avion();
 		Avion a2= new Avion();
 		a1.setAltitude(1000);
