194091ae6b07ee4d7df652d3ec3db27ee9cfb79c
Djebali Nidhal <nidhal.djebali@telecom-paristech.fr>
2015-01-10 12:41:55 +0100

    modifs Avion.java et MonProgrmme.java

--- src/tpgit/Avion.java
+++ src/tpgit/Avion.java
@@ -1,5 +1,14 @@
 package tpgit;
 
 public class Avion {
+	private int altitude=0;
+	public int getAltitude()
+	{
+		return altitude;
+	}
+	public void setAltitude(int altitude)
+	{
+		this.altitude=altitude;
+	}
 
 }
