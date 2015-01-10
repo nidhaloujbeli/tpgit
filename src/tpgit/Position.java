8bf134e0774707e0e13adaf8b4f8bfac2d68c8a4
Djebali Nidhal <nidhal.djebali@telecom-paristech.fr>
2015-01-10 13:00:37 +0100

    Classe de position d'un avion

---
+++ src/tpgit/Position.java
@@ -0,0 +1,27 @@
+package tpgit;
+
+public class Position {
+	private int altitude=0;
+	public int getAltitude() {
+		return altitude;
+	}
+	public void setAltitude(int altitude) {
+		this.altitude = altitude;
+	}
+	public int getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(int latitude) {
+		this.latitude = latitude;
+	}
+	public int getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(int longitude) {
+		this.longitude = longitude;
+	}
+	private int latitude=0;
+	private int longitude=0;
+
+
+}
